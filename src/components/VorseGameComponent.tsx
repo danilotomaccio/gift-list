53aa74c3af7bc5d686706fa0458348be90acbb6a
danilotomaccio <danilo.tomaccio@gmail.com>
2023-10-21 23:08:15 +0200

    Add button blink

--- src/components/VorseGameComponent.tsx
+++ src/components/VorseGameComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MorseAlphabetComponent } from './MorseAlphabetComponent';
 import "./VorseGameComponent.css";
 import { InputPinComponent } from './InputPinComponent';
@@ -6,6 +6,8 @@ import { MdIconComponent } from '../wrappers/icon-wrapper';
 import { MdFilledTonalIconButtonComponent } from '../wrappers/icon-button-wrapper';
 
 export const VorseGameComponent: React.FC<{ onWin: () => void }> = ({ onWin }) => {
+
+    const [flashing, setFlashing] = useState<boolean>(false);
 
     const pwd = "3852";
     const morseCode: { [char: string]: string } = {
@@ -20,11 +22,11 @@ export const VorseGameComponent: React.FC<{ onWin: () => void }> = ({ onWin }) =
         "8": "---..",
         "9": "----.",
         /*  "a": ".-",
-         "b": "-...",
-         "c": "-.-.",
-         "d": "-..",
-         "e": ".",
-         "f": "..-." */
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-." */
     };
 
     const vibrateMorse = () => {
@@ -38,15 +40,26 @@ export const VorseGameComponent: React.FC<{ onWin: () => void }> = ({ onWin }) =
                 pattern.push((j === charMorseCode.length - 1) ? 500 : 200);
             }
         }
-        console.log(pattern);
-        navigator.vibrate(pattern.flat());
+        let sum = 0;
+        
+        for (let i = 0; i < pattern.length; i++) {
+            const cp = sum;
+            setTimeout(() => {
+                setFlashing(i % 2 === 0);
+            }, cp);
+            sum += pattern[i];
+        }
+        navigator.vibrate(pattern);
     };
 
     return (
         <div className='vorseGame'>
-            <MorseAlphabetComponent/>
+            <MorseAlphabetComponent />
             <InputPinComponent pin={pwd} onWin={onWin} />
-            <MdFilledTonalIconButtonComponent onClick={vibrateMorse}>
+            <MdFilledTonalIconButtonComponent onClick={vibrateMorse} style={{
+                '--_container-color': flashing ? 'var(--md-sys-color-error)' : '',
+                '--_icon-color': flashing ? 'var(--md-sys-color-on-error)' : ''
+            }}>
                 <MdIconComponent>vibration</MdIconComponent>
             </MdFilledTonalIconButtonComponent>
         </div>
