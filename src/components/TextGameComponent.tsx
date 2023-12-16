dca6419aae3831d0eef5b3ab9e19ce91cdfa95a6
danilotomaccio <danilo.tomaccio@gmail.com>
2023-12-16 18:02:18 +0100

    Fix initial text

--- src/components/TextGameComponent.tsx
+++ src/components/TextGameComponent.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
 export const TextGameComponent: React.FC<{ onWin: () => void }> = ({ onWin }) => {
-    const text = "Apri quest'applicazione da smartphone. Se sei già su smartphone non ci resta che iniziare. \nSe hai un iPhone, valuta di passare ad Android";
-    const targetWord = "iniziamo";
-    // const targetWord = "i";
+    const text = "Informazione di servizio. Se hai un'officina e vendi trattori, è arrivato il tuo momento! Partecipa al nuovo gioco \"Uno si distrae a Budapest!\". I tuoi amici hanno fiducia in te (tranne Mimmo). Sarai in grado di arrivare fino alla fine. Ma poi, quanti anni fai?!";
+    const targetWord = "trenta";
+
     const initialColors = Array(text.length).fill("inherit");
 
     const [colors, setColors] = useState(initialColors);
@@ -39,7 +39,7 @@ export const TextGameComponent: React.FC<{ onWin: () => void }> = ({ onWin }) =>
             {text.split('').map((letter, index) => (
                 <span
                     key={index}
-                    style={{ userSelect: 'none', color: colors[index], textDecoration: index < 23 && index> 19 ? 'underline' : 'none', cursor: 'pointer' }}
+                    style={{ userSelect: 'none', color: colors[index], textDecoration: index < 12 && index> 8 ? 'underline' : 'none', cursor: 'pointer' }}
                     onClick={() => handleLetterClick(index)}
                 >
                     {letter}
